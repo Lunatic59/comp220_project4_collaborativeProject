cd620a20496facb1676199abdbceb65c7da38170
CALIFFHB24 <califfhb24@gcc.edu>
2025-11-04 17:05:09 -0500

    worked on Cipher class, made it abstract. plus added junit to library.

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,2 +1,9 @@
-public class Cipher {
+public abstract class Cipher {
+    public abstract char encrypt( char c);
+
+    public abstract char decrypt (char c);
+
+    public abstract String encrypt (String s);
+
+    public abstract String decrypt (String s);
 }

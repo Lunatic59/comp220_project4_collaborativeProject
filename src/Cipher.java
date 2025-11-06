076d5f5f464b13027b763b3b0f730b1fa56eed79
Ben Obman <beniaminobman@gmail.com>
2025-11-05 22:51:20 -0500

    Added some Javadoc to Cipher

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,9 +1,27 @@
 public abstract class Cipher {
 
+    /**
+     * Encrypts a single character using the cipher's method
+     *
+     * @param c the character to encrypt
+     * @return the encrypted character
+     */
     public abstract char encrypt( char c);
 
+    /**
+     * Decrypts a single character using the cipher's method
+     *
+     * @param c the character to decrypt
+     * @return the decrypted character
+     */
     public abstract char decrypt (char c);
 
+    /**
+     * Encrypts a string by applying the cipher's {@link #encrypt(char)} method to each character
+     *
+     * @param s the string to encrypt
+     * @return a new string containing the encrypted characters
+     */
     public String encrypt (String s) {
         StringBuilder theString = new StringBuilder();
         theString.append(s);
@@ -14,6 +32,12 @@ public abstract class Cipher {
         return theString.toString();
     }
 
+    /**
+     * Decrypts a string by applying the cipher's {@link #decrypt(char)} method to each character
+     *
+     * @param s the string to decrypt
+     * @return a new string containing the decrypted characters
+     */
     public String decrypt (String s) {
         StringBuilder theString = new StringBuilder();
         theString.append(s);
@@ -24,5 +48,14 @@ public abstract class Cipher {
         return theString.toString();
     }
 
+    /**
+     * Creates and returns a new instance (copy) of this cipher
+     * <p>
+     * This method is used to create a new, uninitialized cipher object
+     * of the same type as the current one
+     * </p>
+     *
+     * @return a new instance of the cipher
+     */
     public abstract Cipher newCopy();
 }
